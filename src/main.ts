83c3cd912a5c0ec67ec7efdc41adce81d615ef7a
Alejandro Parra <alexparra_07@hotmail.com>
2021-05-24 01:49:42 -0500

    fix in naming

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { Pokebattle } from './Pokebattle';
 import { Pokemon } from './Pokemon';
 
 const init = (): void => {
-  let medicham = new Pokemon('durant', 50, 15, 15, 15, 'Bug Bite', ['X-Scissor', 'Stone Edge'], 2);
-  let bastiodon = new Pokemon('blissey', 50, 15, 15, 15, 'Spark', ['Discharge', 'Thunderbolt'], 2);
+  let durant = new Pokemon('durant', 50, 15, 15, 15, 'Bug Bite', ['X-Scissor', 'Stone Edge'], 2);
+  let blissey = new Pokemon('blissey', 50, 15, 15, 15, 'Spark', ['Discharge', 'Thunderbolt'], 2);
 
-  let pokeBattle = new Pokebattle(medicham, bastiodon);
+  let pokeBattle = new Pokebattle(durant, blissey);
 
   //pokeBattle.battle();
   console.log(pokeBattle.calculateAttackDamage(pokeBattle.pokemon1, pokeBattle.pokemon1.data.currentFastMove, pokeBattle.pokemon2));
@@ -14,7 +14,5 @@ const init = (): void => {
   console.log(pokeBattle.maxDamageInTurns(pokeBattle.pokemon1, pokeBattle.pokemon2, 20));
   console.log(pokeBattle.maxDamageInTurns(pokeBattle.pokemon2, pokeBattle.pokemon1, 20));
 }
-
-
 
 window.addEventListener('DOMContentLoaded', init);
